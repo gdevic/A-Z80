23f8dac76ad8dc69ec9637f840959879c0ea75c8
Goran Devic <gdevic@yahoo.com>
2014-05-14 17:38:50 -0500

    z80: Updating the register block

--- cpu/registers/test_latch.sv
+++ cpu/registers/test_latch.sv
@@ -2,8 +2,9 @@
 `timescale 100 ps/ 100 ps
 module test_latch;
 
-reg  [7:0] db;
-wire [7:0] db_sig;
+// Bi-directional bus with 3-state
+reg  [7:0] db;              // Drive it using these wires
+wire [7:0] db_sig;          // Read it using these wires
 
 reg oe_sig;
 reg we_sig;
@@ -11,30 +12,32 @@ reg we_sig;
 initial begin
     oe_sig = 0;
     we_sig = 0;
-    
+
     // Test bidirectional data bus and leave it at Z
     #1 db = 8'hAA;
     #1 db = 'z;
-    
+
     // Write a byte into the latch
-    #1 db = 8'hC0;
+    #1 db = 8'h55;
     #1 we_sig = 1;
     #1 we_sig = 0;
-    
+    #1 db = 'z;
+
     // Read latch
     #1 db = 'z;
     #1 oe_sig = 1;
-    
-    
+    #1 oe_sig = 0;
+
     #1 $display("END");
 
 end
 
+// Drive a 3-state bidirectional bus with this statement
 assign db_sig = db;
 
 reg_latch reg_latch_inst
 (
-	.oe(oe_sig) ,	// input  oe_sig
+	.oen(oe_sig) ,	// input  oe_sig
 	.we(we_sig) ,	// input  we_sig
 	.db(db_sig[7:0]) 	// inout [7:0] db_sig
 );
--- cpu/registers/test_registers.sv
+++ cpu/registers/test_registers.sv
@@ -1,54 +1,71 @@
 // Test register control and register file blocks
-`timescale 1ns/ 100 ps
+`timescale 100 ps/ 100 ps
 module test_registers;
 
-logic [7:0] dbus_lo_as_sig;
-logic [7:0] dbus_hi_as_sig;
+// We have 4 Bi-directional buses that can also be 3-stated:
+// On the address-side, there are high and low 8-bit busses
+reg  [7:0] dbus_lo_as_sig;  // Drive it using this bus
+wire [7:0] dbus_lo_as;      // Read it using this bus
 
-logic [7:0] db_lo_ds_sig;
-logic [7:0] db_hi_ds_sig;
+reg  [7:0] dbus_hi_as_sig;  // Drive it using this bus
+wire [7:0] dbus_hi_as;      // Read it using this bus
 
-reg ctl_sw_4_sig;
+// ----------------- BUSSES -----------------
+// On the data-side, there are high and low 8-bit busses
+reg  [7:0] db_lo_ds_sig;    // Drive it using this bus
+wire [7:0] db_lo_ds;        // Read it using this bus
 
-reg [2:0] reg_sel_sig;
+reg  [7:0] db_hi_ds_sig;    // Drive it using this bus
+wire [7:0] db_hi_ds;        // Read it using this bus
 
-reg ctl_reg_exx_sig;
-reg ctl_reg_ex_af_sig;
-reg ctl_reg_ex_de_hl_sig;
-reg ctl_reg_use_ixiy_sig;
-reg ctl_reg_use_ix_sig;
-reg ctl_reg_use_sp_sig;
-reg ctl_reg_sel_gp_sig;
-reg ctl_reg_sel_wz_sig;
-reg ctl_reg_sel_pc_sig;
-reg ctl_reg_sel_ir_sig;
-reg ctl_reg_sel_sp_sig;
+// ----------------- CONTROL -----------------
+reg ctl_sw_4_sig;           // Bus switch #4
+
+reg ctl_reg_exx_sig;        // Register exchange control
+reg ctl_reg_ex_af_sig;      // Register exchange control
+reg ctl_reg_ex_de_hl_sig;   // Register exchange control
+
+reg ctl_reg_use_ixiy_sig;   // Use IX or IY instead of HL state wire
+reg ctl_reg_use_ix_sig;     // Use IX as opposed to IY
+
+// ----------------- GP REGS -----------------
+reg ctl_reg_sel_gp_sig;     // Select one of the GP registers:
+reg [2:0] reg_sel_sig;      // Selects one of the GP registers: B, C, D, E,...
 reg ctl_reg_sel_gp_16_sig;
-reg ctl_reg_sel_sys_hi_sig;
-reg ctl_reg_sel_sys_lo_sig;
-reg ctl_reg_sys_oe_sig;
-reg ctl_reg_gp_oe_sig;
+reg ctl_reg_sel_sp_sig;     // Use SP instead of AF when indexed through GP selector
+reg ctl_reg_gp_oe_sig;      // Write selected GP register to the data bus
 
-wire reg_sel_bc_sig;
-wire reg_sel_bc2_sig;
-wire reg_sel_ix_sig;
-wire reg_sel_iy_sig;
-wire reg_sel_de_sig;
-wire reg_sel_hl_sig;
-wire reg_sel_de2_sig;
-wire reg_sel_hl2_sig;
+// ----------------- SYSTEM REGS -----------------
+reg ctl_reg_use_sp_sig;     // Direct address system register SP
+reg ctl_reg_sel_wz_sig;     // Direct address system register WZ
+reg ctl_reg_sel_pc_sig;     // Direct address system register PC
+reg ctl_reg_sel_ir_sig;     // Direct address system register IR
+
+reg ctl_reg_sel_sys_hi_sig; // Select high byte of a system register
+reg ctl_reg_sel_sys_lo_sig; // Select low byte of a system register
+reg ctl_reg_sys_oe_sig;     // Write selected system register to the data bus
+
+// These are the output signals from the control module into the register file module
 wire reg_sel_af_sig;
 wire reg_sel_af2_sig;
+wire reg_sel_bc_sig;
+wire reg_sel_bc2_sig;
+wire reg_sel_de_sig;
+wire reg_sel_de2_sig;
+wire reg_sel_hl_sig;
+wire reg_sel_hl2_sig;
+wire reg_sel_ix_sig;
+wire reg_sel_iy_sig;
 wire reg_sel_wz_sig;
+wire reg_sel_sp_sig;
 wire reg_sel_pc_sig;
 wire reg_sel_ir_sig;
-wire reg_sel_sp_sig;
 wire reg_sel_gp_hi_sig;
 wire reg_sel_gp_lo_sig;
 wire reg_sel_sys_hi_sig;
 wire reg_sel_sys_lo_sig;
-wire reg_sys_oe_sig;
 wire reg_gp_oe_sig;
+wire reg_sys_oe_sig;
 
 initial begin
     ctl_sw_4_sig = 1;
@@ -79,6 +96,14 @@ initial begin
 
 end
 
+// Drive 3-state bidirectional buses with these statements
+assign dbus_lo_as_sig = dbus_lo_as;
+assign dbus_hi_as_sig = dbus_hi_as;
+
+assign db_lo_ds_sig = db_lo_ds;
+assign db_hi_ds_sig = db_hi_ds;
+
+// Instantiate register control block
 reg_control reg_control_inst
 (
 	.reg_sel(reg_sel_sig) ,	// input [2:0] reg_sel_sig
@@ -120,6 +145,7 @@ reg_control reg_control_inst
 	.reg_gp_oe(reg_gp_oe_sig) 	// output  reg_gp_oe_sig
 );
 
+// Instantiate register file block
 reg_file reg_file_inst
 (
 	.ctl_sw_4(ctl_sw_4_sig) ,	// input  ctl_sw_4_sig
