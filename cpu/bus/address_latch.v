736132b8849c91026c09c66d38d052f5a76b9f69
Goran Devic <gdevic@yahoo.com>
2014-05-17 01:49:21 -0500

    z80: Completed address latch/incrementer

---
+++ cpu/bus/address_latch.v
@@ -0,0 +1,101 @@
+// Copyright (C) 1991-2011 Altera Corporation
+// Your use of Altera Corporation's design tools, logic functions 
+// and other software and tools, and its AMPP partner logic 
+// functions, and any output files from any of the foregoing 
+// (including device programming or simulation files), and any 
+// associated documentation or information are expressly subject 
+// to the terms and conditions of the Altera Program License 
+// Subscription Agreement, Altera MegaCore Function License 
+// Agreement, or other applicable license agreement, including, 
+// without limitation, that your use is for the sole purpose of 
+// programming logic devices manufactured by Altera and sold by 
+// Altera or its authorized distributors.  Please refer to the 
+// applicable agreement for further details.
+
+// PROGRAM		"Quartus II 64-Bit"
+// VERSION		"Version 11.0 Build 208 07/03/2011 Service Pack 1 SJ Full Version"
+// CREATED		"Sat May 17 01:20:37 2014"
+
+module address_latch(
+	ctl_bus_inc_we,
+	ctl_inc_cy,
+	ctl_inc_dec,
+	ctl_inc_zero,
+	ctl_al_we,
+	ctl_ab_mux_inc,
+	ctl_inc_limit6,
+	address_is_1,
+	address,
+	db
+);
+
+
+input wire	ctl_bus_inc_we;
+input wire	ctl_inc_cy;
+input wire	ctl_inc_dec;
+input wire	ctl_inc_zero;
+input wire	ctl_al_we;
+input wire	ctl_ab_mux_inc;
+input wire	ctl_inc_limit6;
+output wire	address_is_1;
+output wire	[15:0] address;
+inout wire	[15:0] db;
+
+wire	SYNTHESIZED_WIRE_0;
+reg	[15:0] SYNTHESIZED_WIRE_9;
+wire	[15:0] SYNTHESIZED_WIRE_10;
+wire	[15:0] SYNTHESIZED_WIRE_3;
+wire	[15:0] SYNTHESIZED_WIRE_4;
+wire	SYNTHESIZED_WIRE_5;
+wire	[15:0] SYNTHESIZED_WIRE_7;
+
+
+
+
+assign	SYNTHESIZED_WIRE_4 = {SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0,SYNTHESIZED_WIRE_0} & SYNTHESIZED_WIRE_9;
+
+assign	SYNTHESIZED_WIRE_3 = {ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc} & SYNTHESIZED_WIRE_10;
+
+assign	address = SYNTHESIZED_WIRE_3 | SYNTHESIZED_WIRE_4;
+
+assign	SYNTHESIZED_WIRE_0 =  ~ctl_ab_mux_inc;
+
+assign	SYNTHESIZED_WIRE_5 =  ~ctl_inc_zero;
+
+assign	SYNTHESIZED_WIRE_7 = {SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5} & SYNTHESIZED_WIRE_9;
+
+assign	db[15] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[15] : 1'bz;
+assign	db[14] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[14] : 1'bz;
+assign	db[13] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[13] : 1'bz;
+assign	db[12] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[12] : 1'bz;
+assign	db[11] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[11] : 1'bz;
+assign	db[10] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[10] : 1'bz;
+assign	db[9] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[9] : 1'bz;
+assign	db[8] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[8] : 1'bz;
+assign	db[7] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[7] : 1'bz;
+assign	db[6] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[6] : 1'bz;
+assign	db[5] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[5] : 1'bz;
+assign	db[4] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[4] : 1'bz;
+assign	db[3] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[3] : 1'bz;
+assign	db[2] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[2] : 1'bz;
+assign	db[1] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[1] : 1'bz;
+assign	db[0] = ctl_bus_inc_we ? SYNTHESIZED_WIRE_7[0] : 1'bz;
+
+
+always@(ctl_al_we or SYNTHESIZED_WIRE_10)
+begin
+if (ctl_al_we)
+	SYNTHESIZED_WIRE_9 <= SYNTHESIZED_WIRE_10;
+end
+
+
+inc_dec	b2v_inst_inc_dec(
+	.limit6(ctl_inc_limit6),
+	.decrement(ctl_inc_dec),
+	.carry_in(ctl_inc_cy),
+	.d(db),
+	.address_is_1(address_is_1),
+	.address(SYNTHESIZED_WIRE_10));
+
+
+endmodule
---
+++ cpu/bus/test_bus.sv
@@ -0,0 +1,91 @@
+// Test address latch and increment block
+`timescale 100 ps/ 100 ps
+module test_bus;
+
+// Bi-directional bus that can also be tri-stated
+reg  [15:0] db;             // Drive it using this bus
+wire [15:0] db_sig;         // Read it using this bus
+
+wire [15:0] address_sig;    // Final address ouput
+
+// ----------------- INPUT CONTROL -----------------
+reg ctl_ab_mux_inc_sig;     // Mux for the final address output
+reg ctl_al_we_sig;          // Write enable to address latch
+reg ctl_bus_inc_we_sig;     // Write incrementer onto the internal data bus
+
+// ----------------- INC/DEC -----------------
+reg ctl_inc_dec_sig;        // Perform decrement (1) or increment (0)
+reg ctl_inc_limit6_sig;     // Limit increment to 6 bits (for incrementing IR)
+reg ctl_inc_cy_sig;         // Address increment, carry in value (+/-1 or 0)
+reg ctl_inc_zero_sig;       // Output zero from the incrementer
+
+// ----------------- OUTPUT/STATUS -----------------
+wire address_is_1_sig;      // Signals when the final address is 1
+
+initial begin
+    db = 'z;
+    ctl_ab_mux_inc_sig = 0;
+    ctl_al_we_sig = 0;
+    ctl_bus_inc_we_sig = 0;
+    ctl_inc_dec_sig = 0;
+    ctl_inc_limit6_sig = 0;
+    ctl_inc_cy_sig = 0;
+    ctl_inc_zero_sig = 0;
+
+    //------------------------------------------------------------
+    // Perform a simple increment and decrement
+    #1  db = 16'h0;
+        ctl_ab_mux_inc_sig = 1;     // Output incrementer to the address bus
+        ctl_inc_cy_sig = 1;         // +1  show "1"
+    #1  ctl_inc_dec_sig = 1;        // -1  show "FFFF"
+    // ...through overflow
+    #1  db = 16'hffff;
+        ctl_inc_dec_sig = 0;
+        ctl_inc_cy_sig = 1;         // +1  show "0"
+    #1  ctl_inc_dec_sig = 1;        // -1  show "FFFE"
+    // test pass-through
+    #1  ctl_inc_cy_sig = 0;         // show "FFFF"
+    #1  ctl_inc_dec_sig = 0;
+
+    //------------------------------------------------------------
+    // Test the address latch and the mux
+    #1  db = 16'hAA55;
+        ctl_al_we_sig = 1;          // Write AA55 to the latch
+        ctl_inc_cy_sig = 0;
+    #1  db = 16'hCAFE;              // Output CAFE through the incrementer
+        ctl_al_we_sig = 0;          // show "CAFE"
+    #1  ctl_ab_mux_inc_sig = 0;     // show "AA55"
+
+    //------------------------------------------------------------
+    // Test the tri-state db
+    #1  db = 'z;
+        ctl_bus_inc_we_sig = 1;     // Output latched value (AA55)
+    #1  ctl_bus_inc_we_sig = 0;     // Disable it for a cycle
+        
+    //------------------------------------------------------------
+    // Test the force-to-zero
+    #1  ctl_inc_zero_sig = 1;
+        ctl_bus_inc_we_sig = 1;     // Output zero
+
+    #1 $display("End of test");
+end
+
+// Drive 3-state bidirectional bus with these statements
+assign db_sig = db;
+
+// Instantiate address latch block
+address_latch address_latch_inst
+(
+	.ctl_bus_inc_we(ctl_bus_inc_we_sig) ,	// input  ctl_bus_inc_we_sig
+	.ctl_inc_limit6(ctl_inc_limit6_sig) ,	// input  ctl_inc_limit6_sig
+	.ctl_inc_cy(ctl_inc_cy_sig) ,	// input  ctl_inc_cy_sig
+	.ctl_inc_dec(ctl_inc_dec_sig) ,	// input  ctl_inc_dec_sig
+	.ctl_inc_zero(ctl_inc_zero_sig) ,	// input  ctl_inc_zero_sig
+	.ctl_al_we(ctl_al_we_sig) ,	// input  ctl_al_we_sig
+	.ctl_ab_mux_inc(ctl_ab_mux_inc_sig) ,	// input  ctl_ab_mux_inc_sig
+	.address(address_sig[15:0]) ,	// output [15:0] address_sig
+	.address_is_1(address_is_1_sig) ,	// output  address_is_1_sig
+	.db(db_sig[15:0]) 	// inout [15:0] db_sig
+);
+
+endmodule
