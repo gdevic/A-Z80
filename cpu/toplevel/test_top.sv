03c44551f5d08c9d61d1a227d0ed5f3d2b4cadd2
Goran Devic <gdevic@yahoo.com>
2014-07-11 12:53:01 -0500

    z80: Updated top level

--- cpu/toplevel/test_top.sv
+++ cpu/toplevel/test_top.sv
@@ -2,25 +2,24 @@
 // Testbench for the top level design
 //--------------------------------------------------------------
 `timescale 100 ps/ 100 ps
+`define TD  #1
 
 `include "z80.svh"
 
 module test_bench(intf.tb tb_if);
 
+assign clk = tb_if.CPUCLK;
+
 initial begin
-    tb_if.nINT = 0;
-    tb_if.nNMI = 0;
-    tb_if.nRESET = 1;
-    tb_if.nBUSRQ = 0;
+    $display("Test: Start of test at %d", $time);
+    tb_if.nINT <= 0;
+    tb_if.nNMI <= 0;
+    tb_if.nRESET <= 1;
+    tb_if.nBUSRQ <= 0;
 
-    #2 tb_if.nRESET = 0;
+    repeat (3) @(posedge clk);
+    tb_if.nRESET <= 0;
 
-    #100 $display("End of test");
-    $stop;
-end
-
-final begin
-    $display("Final: End of test at %d", $time);
 end
 
 endmodule
@@ -28,10 +27,13 @@ endmodule
 module test_top();
 
 logic clk = 0;
-always #1 clk++;
+initial repeat (20) `TD clk = ~clk;
 
-intf bus_if(clk);
-z80_top d(bus_if);
-test_bench tb(bus_if);
+// Instantiate the bus interface to the design
+intf        bus_if(clk);
+// Create an instance of our Z80 design
+z80_top     d(bus_if);
+// Create an instance of the test bench
+test_bench  tb(bus_if);
 
 endmodule
