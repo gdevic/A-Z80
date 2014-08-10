ceb8dd87ddeff9b7fcb4ac93b83628b4e2890d46
Goran Devic <gdevic@yahoo.com>
2014-08-09 23:44:42 -0500

    z80: Made the sequencer self-starting after a reset

--- cpu/control/sequencer.v
+++ cpu/control/sequencer.v
@@ -14,7 +14,7 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 11.0 Build 208 07/03/2011 Service Pack 1 SJ Full Version"
-// CREATED		"Tue Jul 22 20:11:04 2014"
+// CREATED		"Sat Aug 09 23:41:57 2014"
 
 module sequencer(
 	clk,
@@ -57,332 +57,334 @@ output wire	T4;
 output wire	T5;
 output reg	T6;
 
-reg	SYNTHESIZED_WIRE_64;
-reg	SYNTHESIZED_WIRE_65;
-reg	SYNTHESIZED_WIRE_1;
 reg	SYNTHESIZED_WIRE_66;
-wire	SYNTHESIZED_WIRE_67;
-reg	DFFE_instM1;
+reg	SYNTHESIZED_WIRE_67;
+reg	SYNTHESIZED_WIRE_1;
+reg	SYNTHESIZED_WIRE_68;
+wire	SYNTHESIZED_WIRE_69;
+reg	SYNTHESIZED_WIRE_70;
+wire	SYNTHESIZED_WIRE_71;
 reg	DFFE_instM2;
 reg	DFFE_instM3;
 reg	DFFE_instM4;
 reg	DFFE_instM5;
-wire	SYNTHESIZED_WIRE_68;
-reg	DFFE_instT1;
+wire	SYNTHESIZED_WIRE_72;
+reg	SYNTHESIZED_WIRE_73;
 reg	DFFE_instT3;
 reg	DFFE_instT4;
 reg	DFFE_instT5;
-wire	SYNTHESIZED_WIRE_69;
-wire	SYNTHESIZED_WIRE_70;
-wire	SYNTHESIZED_WIRE_19;
-wire	SYNTHESIZED_WIRE_23;
-wire	SYNTHESIZED_WIRE_27;
-wire	SYNTHESIZED_WIRE_31;
-wire	SYNTHESIZED_WIRE_35;
-wire	SYNTHESIZED_WIRE_71;
-wire	SYNTHESIZED_WIRE_43;
-wire	SYNTHESIZED_WIRE_47;
-wire	SYNTHESIZED_WIRE_51;
-wire	SYNTHESIZED_WIRE_55;
-wire	SYNTHESIZED_WIRE_59;
-wire	SYNTHESIZED_WIRE_72;
-wire	SYNTHESIZED_WIRE_62;
+wire	SYNTHESIZED_WIRE_74;
+wire	SYNTHESIZED_WIRE_21;
+wire	SYNTHESIZED_WIRE_25;
+wire	SYNTHESIZED_WIRE_29;
+wire	SYNTHESIZED_WIRE_33;
+wire	SYNTHESIZED_WIRE_37;
+wire	SYNTHESIZED_WIRE_75;
+wire	SYNTHESIZED_WIRE_45;
+wire	SYNTHESIZED_WIRE_49;
+wire	SYNTHESIZED_WIRE_53;
+wire	SYNTHESIZED_WIRE_57;
+wire	SYNTHESIZED_WIRE_61;
+wire	SYNTHESIZED_WIRE_76;
+wire	SYNTHESIZED_WIRE_64;
 
-assign	M1 = DFFE_instM1;
 assign	M2 = DFFE_instM2;
 assign	M3 = DFFE_instM3;
 assign	M4 = DFFE_instM4;
 assign	M5 = DFFE_instM5;
-assign	T1 = DFFE_instT1;
 assign	T3 = DFFE_instT3;
 assign	T4 = DFFE_instT4;
 assign	T5 = DFFE_instT5;
-assign	SYNTHESIZED_WIRE_69 = 1;
+assign	SYNTHESIZED_WIRE_74 = 1;
 
 
 
-assign	T2 = T2_en & SYNTHESIZED_WIRE_64;
+assign	T2 = T2_en & SYNTHESIZED_WIRE_66;
 
-assign	SYNTHESIZED_WIRE_67 =  ~SYNTHESIZED_WIRE_65;
+assign	SYNTHESIZED_WIRE_69 =  ~SYNTHESIZED_WIRE_67;
 
-assign	SYNTHESIZED_WIRE_70 =  ~reset;
+assign	SYNTHESIZED_WIRE_71 =  ~reset;
 
-assign	SYNTHESIZED_WIRE_62 =  ~clk;
+assign	SYNTHESIZED_WIRE_64 =  ~clk;
 
-assign	SYNTHESIZED_WIRE_71 =  ~SYNTHESIZED_WIRE_1;
+assign	SYNTHESIZED_WIRE_75 =  ~SYNTHESIZED_WIRE_1;
 
-assign	SYNTHESIZED_WIRE_72 =  ~clk;
+assign	SYNTHESIZED_WIRE_76 =  ~clk;
 
-assign	SYNTHESIZED_WIRE_68 =  ~SYNTHESIZED_WIRE_66;
+assign	SYNTHESIZED_WIRE_72 =  ~SYNTHESIZED_WIRE_68;
 
-assign	SYNTHESIZED_WIRE_19 = SYNTHESIZED_WIRE_67 & DFFE_instM1;
+assign	SYNTHESIZED_WIRE_21 = SYNTHESIZED_WIRE_69 & SYNTHESIZED_WIRE_70;
 
-assign	SYNTHESIZED_WIRE_23 = SYNTHESIZED_WIRE_67 & DFFE_instM2;
+assign	M1 = SYNTHESIZED_WIRE_70 & SYNTHESIZED_WIRE_71;
 
-assign	SYNTHESIZED_WIRE_27 = SYNTHESIZED_WIRE_67 & DFFE_instM3;
+assign	SYNTHESIZED_WIRE_25 = SYNTHESIZED_WIRE_69 & DFFE_instM2;
 
-assign	SYNTHESIZED_WIRE_31 = SYNTHESIZED_WIRE_67 & DFFE_instM4;
+assign	SYNTHESIZED_WIRE_29 = SYNTHESIZED_WIRE_69 & DFFE_instM3;
 
-assign	SYNTHESIZED_WIRE_35 = SYNTHESIZED_WIRE_67 & DFFE_instM5;
+assign	SYNTHESIZED_WIRE_33 = SYNTHESIZED_WIRE_69 & DFFE_instM4;
 
-
-assign	SYNTHESIZED_WIRE_43 = SYNTHESIZED_WIRE_68 & DFFE_instT1;
-
-assign	SYNTHESIZED_WIRE_47 = SYNTHESIZED_WIRE_68 & SYNTHESIZED_WIRE_64;
-
-assign	SYNTHESIZED_WIRE_51 = SYNTHESIZED_WIRE_68 & DFFE_instT3;
-
-assign	SYNTHESIZED_WIRE_55 = SYNTHESIZED_WIRE_68 & DFFE_instT4;
-
-assign	SYNTHESIZED_WIRE_59 = SYNTHESIZED_WIRE_68 & DFFE_instT5;
+assign	SYNTHESIZED_WIRE_37 = SYNTHESIZED_WIRE_69 & DFFE_instM5;
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+assign	SYNTHESIZED_WIRE_45 = SYNTHESIZED_WIRE_72 & SYNTHESIZED_WIRE_73;
+
+assign	T1 = SYNTHESIZED_WIRE_71 & SYNTHESIZED_WIRE_73;
+
+assign	SYNTHESIZED_WIRE_49 = SYNTHESIZED_WIRE_72 & SYNTHESIZED_WIRE_66;
+
+assign	SYNTHESIZED_WIRE_53 = SYNTHESIZED_WIRE_72 & DFFE_instT3;
+
+assign	SYNTHESIZED_WIRE_57 = SYNTHESIZED_WIRE_72 & DFFE_instT4;
+
+assign	SYNTHESIZED_WIRE_61 = SYNTHESIZED_WIRE_72 & DFFE_instT5;
+
+
+always@(posedge clk or negedge SYNTHESIZED_WIRE_74 or negedge SYNTHESIZED_WIRE_71)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_74)
 	begin
-	DFFE_instM1 <= 0;
+	SYNTHESIZED_WIRE_70 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_71)
 	begin
-	DFFE_instM1 <= 1;
+	SYNTHESIZED_WIRE_70 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_66)
+if (SYNTHESIZED_WIRE_68)
 	begin
-	DFFE_instM1 <= SYNTHESIZED_WIRE_65;
+	SYNTHESIZED_WIRE_70 <= SYNTHESIZED_WIRE_67;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
 	DFFE_instM2 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
 	DFFE_instM2 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_66)
+if (SYNTHESIZED_WIRE_68)
 	begin
-	DFFE_instM2 <= SYNTHESIZED_WIRE_19;
+	DFFE_instM2 <= SYNTHESIZED_WIRE_21;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
 	DFFE_instM3 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
 	DFFE_instM3 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_66)
+if (SYNTHESIZED_WIRE_68)
 	begin
-	DFFE_instM3 <= SYNTHESIZED_WIRE_23;
+	DFFE_instM3 <= SYNTHESIZED_WIRE_25;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
 	DFFE_instM4 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
 	DFFE_instM4 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_66)
+if (SYNTHESIZED_WIRE_68)
 	begin
-	DFFE_instM4 <= SYNTHESIZED_WIRE_27;
+	DFFE_instM4 <= SYNTHESIZED_WIRE_29;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
 	DFFE_instM5 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
 	DFFE_instM5 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_66)
+if (SYNTHESIZED_WIRE_68)
 	begin
-	DFFE_instM5 <= SYNTHESIZED_WIRE_31;
+	DFFE_instM5 <= SYNTHESIZED_WIRE_33;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
 	M6 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
 	M6 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_66)
+if (SYNTHESIZED_WIRE_68)
 	begin
-	M6 <= SYNTHESIZED_WIRE_35;
+	M6 <= SYNTHESIZED_WIRE_37;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_74 or negedge SYNTHESIZED_WIRE_71)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_74)
 	begin
-	DFFE_instT1 <= 0;
+	SYNTHESIZED_WIRE_73 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_71)
 	begin
-	DFFE_instT1 <= 1;
+	SYNTHESIZED_WIRE_73 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_71)
+if (SYNTHESIZED_WIRE_75)
 	begin
-	DFFE_instT1 <= SYNTHESIZED_WIRE_66;
+	SYNTHESIZED_WIRE_73 <= SYNTHESIZED_WIRE_68;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
-	SYNTHESIZED_WIRE_64 <= 0;
+	SYNTHESIZED_WIRE_66 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
-	SYNTHESIZED_WIRE_64 <= 1;
+	SYNTHESIZED_WIRE_66 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_71)
+if (SYNTHESIZED_WIRE_75)
 	begin
-	SYNTHESIZED_WIRE_64 <= SYNTHESIZED_WIRE_43;
+	SYNTHESIZED_WIRE_66 <= SYNTHESIZED_WIRE_45;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
 	DFFE_instT3 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
 	DFFE_instT3 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_71)
+if (SYNTHESIZED_WIRE_75)
 	begin
-	DFFE_instT3 <= SYNTHESIZED_WIRE_47;
+	DFFE_instT3 <= SYNTHESIZED_WIRE_49;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
 	DFFE_instT4 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
 	DFFE_instT4 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_71)
+if (SYNTHESIZED_WIRE_75)
 	begin
-	DFFE_instT4 <= SYNTHESIZED_WIRE_51;
+	DFFE_instT4 <= SYNTHESIZED_WIRE_53;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
 	DFFE_instT5 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
 	DFFE_instT5 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_71)
+if (SYNTHESIZED_WIRE_75)
 	begin
-	DFFE_instT5 <= SYNTHESIZED_WIRE_55;
+	DFFE_instT5 <= SYNTHESIZED_WIRE_57;
 	end
 end
 
 
-always@(posedge clk or negedge SYNTHESIZED_WIRE_70 or negedge SYNTHESIZED_WIRE_69)
+always@(posedge clk or negedge SYNTHESIZED_WIRE_71 or negedge SYNTHESIZED_WIRE_74)
 begin
-if (!SYNTHESIZED_WIRE_70)
+if (!SYNTHESIZED_WIRE_71)
 	begin
 	T6 <= 0;
 	end
 else
-if (!SYNTHESIZED_WIRE_69)
+if (!SYNTHESIZED_WIRE_74)
 	begin
 	T6 <= 1;
 	end
 else
-if (SYNTHESIZED_WIRE_71)
+if (SYNTHESIZED_WIRE_75)
 	begin
-	T6 <= SYNTHESIZED_WIRE_59;
+	T6 <= SYNTHESIZED_WIRE_61;
 	end
 end
 
 
-always@(SYNTHESIZED_WIRE_72 or hold_clk)
+always@(SYNTHESIZED_WIRE_76 or hold_clk)
 begin
-if (SYNTHESIZED_WIRE_72)
+if (SYNTHESIZED_WIRE_76)
 	SYNTHESIZED_WIRE_1 <= hold_clk;
 end
 
 
-always@(SYNTHESIZED_WIRE_62 or setM1)
+always@(SYNTHESIZED_WIRE_64 or setM1)
 begin
-if (SYNTHESIZED_WIRE_62)
-	SYNTHESIZED_WIRE_65 <= setM1;
+if (SYNTHESIZED_WIRE_64)
+	SYNTHESIZED_WIRE_67 <= setM1;
 end
 
 
-always@(SYNTHESIZED_WIRE_72 or nextM)
+always@(SYNTHESIZED_WIRE_76 or nextM)
 begin
-if (SYNTHESIZED_WIRE_72)
-	SYNTHESIZED_WIRE_66 <= nextM;
+if (SYNTHESIZED_WIRE_76)
+	SYNTHESIZED_WIRE_68 <= nextM;
 end
 
 
