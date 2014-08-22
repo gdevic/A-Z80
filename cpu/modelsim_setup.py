0182212f7ae192e6770363b542d65c1a1d7a418d
Goran Devic <gdevic@yahoo.com>
2014-08-22 14:14:01 -0500

    z80: Simplified ModelSim setup
    - Removed unnecessary folder setup
    - With clean repo, simply create a library by issuing:
    > vlib work
    Then compile all the sources.

--- cpu/modelsim_setup.py
+++ cpu/modelsim_setup.py
@@ -2,32 +2,19 @@
 #
 # This script sets up the environment to run ModelSim on each module.
 #
-# (1) ModelSim needs existing project library folders in order to compile
-# modules but it can't create them (!). This script creates those directories
-# instead of having to check in empty folders.
-# (2) Sets up a relative path to your specific directory mapping by creating
+# It sets up a relative path to your specific directory mapping by creating
 # a file "mgc_location_map". We use loction mapping, and all paths to source
 # files are relative.
 #
 import os
 
 def setup():
-    # Create a set of library folders
-    with open("_info") as f:
-        for line in f:
-            # When a ModelSim library information file starts with:
-            # v : the rest of the line contains a name of a module
-            # Y : the rest of the line contains a name of an interface
-            if (line.startswith("v") or line.startswith("Y")) and not os.path.exists(line[1:].strip()):
-                print line[1:].strip()
-                os.mkdir(line[1:].strip())
-
     # Create mgc_location_map with relative path mapping
     # Assumes this directory hierarchy:
     # $ROOT/<module>/simulation/modelsim/work/<this script>.py
-    with open("../mgc_location_map", "w") as f:
+    with open("mgc_location_map", "w") as f:
         f.write("$ROOT\n")
-        path = os.path.abspath("../../../.")
+        path = os.path.abspath("../../.")
         f.write(os.path.dirname(path))
 
 # Return to our current directory after each module has been visited
@@ -35,22 +22,22 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 
 # Visit each ModelSim project directory...
-os.chdir("alu/simulation/modelsim/work")
+os.chdir("alu/simulation/modelsim")
 setup()
 os.chdir(dname)
 
-os.chdir("bus/simulation/modelsim/work")
+os.chdir("bus/simulation/modelsim")
 setup()
 os.chdir(dname)
 
-os.chdir("control/simulation/modelsim/work")
+os.chdir("control/simulation/modelsim")
 setup()
 os.chdir(dname)
 
-os.chdir("registers/simulation/modelsim/work")
+os.chdir("registers/simulation/modelsim")
 setup()
 os.chdir(dname)
 
-os.chdir("toplevel/simulation/modelsim/work")
+os.chdir("toplevel/simulation/modelsim")
 setup()
 os.chdir(dname)
--- cpu/readme.txt
+++ cpu/readme.txt
@@ -11,7 +11,7 @@ Prerequisites:
 
 A-Z80 consists of several functional blocks and a top-level module:
 ./alu       contains ALU block, ALU control and flags logic
-./bus       contains data bus switches, chip pin logic, address latch and incrementer
+./bus       contains data bus switches, pin logic, address latch and incrementer
 ./register  contains CPU register file and register control logic
 ./control   contains PLA, sequencer and other control blocks
 ./toplevel  integrates all sub-modules into a top-level design
@@ -29,18 +29,20 @@ exported to Verilog for simulation and the top-level integration.
 
 Simulation
 ==========
-Each functional block contains a Modelsim simulation profile:
+Before you can load and simulate modules using Modelsim, you need to set up
+the environment by running a Python script 'modelsim_setup.py'. It will create
+relative file path mapping to the source files in all module project folders.
+
+Each functional block - and the top level - contains a Modelsim simulation profile:
 ./<block>/simulation/modelsim/test_<block>.mpf
 
-Before you can load and simulate modules using Modelsim, you need to set up
-the environment by running a Python script 'modelsim_setup.py'.
+Once you start ModelSim by opening a *.mpf file within a module, create a library:
+ModelSim> vlib work
+After that you can compile all module/project sources (Compile->Compile All).
 
-Each Modelsim project contains a set of predefined waveform scripts which you can
-load before running a simulation of a particular submodule. The scripts are named
-according to a sub-module to test:
-./<block>/simulation/modelsim/wave_<submodule>.do
-
-All tests and test benches are written in SystemVerilog (and have extension *.sv).
+Each project also contains a set of predefined waveform scripts which you can
+load before running a simulation of a particular test:
+./<block>/simulation/modelsim/wave_<test>.do
 
 ----------------------------------------------------------------------------------
 Email me if you have any questions,
