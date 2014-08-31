0bc67a47ccdccc75e161fe6b1e7797c4e9ff07d6
Goran Devic <gdevic@yahoo.com>
2014-08-31 12:21:06 -0500

    z80: Correcting the location of address=1 check

--- cpu/bus/address_latch.v
+++ cpu/bus/address_latch.v
@@ -14,7 +14,7 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 11.0 Build 208 07/03/2011 Service Pack 1 SJ Full Version"
-// CREATED		"Sun Aug 10 12:12:19 2014"
+// CREATED		"Sun Aug 31 12:16:22 2014"
 
 module address_latch(
 	ctl_inc_cy,
@@ -41,51 +41,62 @@ output wire	address_is_1;
 inout wire	[15:0] abus;
 output wire	[15:0] address;
 
-wire	[15:0] SYNTHESIZED_WIRE_9;
+reg	[15:0] q;
+wire	[15:0] SYNTHESIZED_WIRE_10;
 wire	SYNTHESIZED_WIRE_1;
-reg	[15:0] SYNTHESIZED_WIRE_10;
+wire	[15:0] SYNTHESIZED_WIRE_2;
 wire	[15:0] SYNTHESIZED_WIRE_3;
-wire	[15:0] SYNTHESIZED_WIRE_4;
+wire	SYNTHESIZED_WIRE_4;
 wire	SYNTHESIZED_WIRE_5;
-wire	[15:0] SYNTHESIZED_WIRE_7;
+wire	SYNTHESIZED_WIRE_6;
+wire	[15:0] SYNTHESIZED_WIRE_8;
+wire	SYNTHESIZED_WIRE_9;
 
 
 
 
-assign	SYNTHESIZED_WIRE_4 = {ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc} & SYNTHESIZED_WIRE_9;
+assign	SYNTHESIZED_WIRE_3 = {ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc,ctl_ab_mux_inc} & SYNTHESIZED_WIRE_10;
 
-assign	SYNTHESIZED_WIRE_3 = {SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1} & SYNTHESIZED_WIRE_10;
+assign	SYNTHESIZED_WIRE_2 = {SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1,SYNTHESIZED_WIRE_1} & q;
 
-assign	address = SYNTHESIZED_WIRE_3 | SYNTHESIZED_WIRE_4;
+assign	address = SYNTHESIZED_WIRE_2 | SYNTHESIZED_WIRE_3;
 
 assign	SYNTHESIZED_WIRE_1 =  ~ctl_ab_mux_inc;
 
-assign	SYNTHESIZED_WIRE_5 =  ~ctl_inc_zero;
+assign	SYNTHESIZED_WIRE_6 =  ~ctl_inc_zero;
 
-assign	SYNTHESIZED_WIRE_7 = {SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5,SYNTHESIZED_WIRE_5} & SYNTHESIZED_WIRE_9;
+assign	address_is_1 = ~(SYNTHESIZED_WIRE_4 | SYNTHESIZED_WIRE_5);
 
-assign	abus[15] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[15] : 1'bz;
-assign	abus[14] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[14] : 1'bz;
-assign	abus[13] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[13] : 1'bz;
-assign	abus[12] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[12] : 1'bz;
-assign	abus[11] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[11] : 1'bz;
-assign	abus[10] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[10] : 1'bz;
-assign	abus[9] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[9] : 1'bz;
-assign	abus[8] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[8] : 1'bz;
-assign	abus[7] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[7] : 1'bz;
-assign	abus[6] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[6] : 1'bz;
-assign	abus[5] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[5] : 1'bz;
-assign	abus[4] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[4] : 1'bz;
-assign	abus[3] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[3] : 1'bz;
-assign	abus[2] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[2] : 1'bz;
-assign	abus[1] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[1] : 1'bz;
-assign	abus[0] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_7[0] : 1'bz;
+assign	SYNTHESIZED_WIRE_9 =  ~q[0];
+
+assign	SYNTHESIZED_WIRE_8 = {SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6,SYNTHESIZED_WIRE_6} & SYNTHESIZED_WIRE_10;
+
+assign	abus[15] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[15] : 1'bz;
+assign	abus[14] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[14] : 1'bz;
+assign	abus[13] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[13] : 1'bz;
+assign	abus[12] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[12] : 1'bz;
+assign	abus[11] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[11] : 1'bz;
+assign	abus[10] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[10] : 1'bz;
+assign	abus[9] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[9] : 1'bz;
+assign	abus[8] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[8] : 1'bz;
+assign	abus[7] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[7] : 1'bz;
+assign	abus[6] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[6] : 1'bz;
+assign	abus[5] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[5] : 1'bz;
+assign	abus[4] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[4] : 1'bz;
+assign	abus[3] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[3] : 1'bz;
+assign	abus[2] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[2] : 1'bz;
+assign	abus[1] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[1] : 1'bz;
+assign	abus[0] = ctl_bus_inc_oe ? SYNTHESIZED_WIRE_8[0] : 1'bz;
+
+assign	SYNTHESIZED_WIRE_4 = q[7] | q[5] | q[6] | q[4] | q[2] | q[3] | q[1] | SYNTHESIZED_WIRE_9;
+
+assign	SYNTHESIZED_WIRE_5 = q[15] | q[13] | q[14] | q[12] | q[10] | q[11] | q[9] | q[8];
 
 
 always@(ctl_al_we or abus)
 begin
 if (ctl_al_we)
-	SYNTHESIZED_WIRE_10 <= abus;
+	q <= abus;
 end
 
 
@@ -93,9 +104,8 @@ inc_dec	b2v_inst_inc_dec(
 	.limit6(ctl_inc_limit6),
 	.decrement(ctl_inc_dec),
 	.carry_in(ctl_inc_cy),
-	.d(SYNTHESIZED_WIRE_10),
-	.address_is_1(address_is_1),
-	.address(SYNTHESIZED_WIRE_9));
+	.d(q),
+	.address(SYNTHESIZED_WIRE_10));
 
 
 endmodule
--- cpu/bus/inc_dec.v
+++ cpu/bus/inc_dec.v
@@ -14,14 +14,13 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 11.0 Build 208 07/03/2011 Service Pack 1 SJ Full Version"
-// CREATED		"Sun Jul 06 08:30:50 2014"
+// CREATED		"Sun Aug 31 12:19:59 2014"
 
 module inc_dec(
 	carry_in,
 	limit6,
 	decrement,
 	d,
-	address_is_1,
 	address
 );
 
@@ -30,26 +29,25 @@ input wire	carry_in;
 input wire	limit6;
 input wire	decrement;
 input wire	[15:0] d;
-output wire	address_is_1;
 output wire	[15:0] address;
 
 wire	[15:0] address_ALTERA_SYNTHESIZED;
+wire	SYNTHESIZED_WIRE_40;
+wire	SYNTHESIZED_WIRE_41;
+wire	SYNTHESIZED_WIRE_42;
 wire	SYNTHESIZED_WIRE_43;
 wire	SYNTHESIZED_WIRE_44;
+wire	SYNTHESIZED_WIRE_5;
 wire	SYNTHESIZED_WIRE_45;
 wire	SYNTHESIZED_WIRE_46;
 wire	SYNTHESIZED_WIRE_47;
-wire	SYNTHESIZED_WIRE_5;
 wire	SYNTHESIZED_WIRE_48;
 wire	SYNTHESIZED_WIRE_49;
 wire	SYNTHESIZED_WIRE_50;
+wire	SYNTHESIZED_WIRE_12;
 wire	SYNTHESIZED_WIRE_51;
 wire	SYNTHESIZED_WIRE_52;
 wire	SYNTHESIZED_WIRE_53;
-wire	SYNTHESIZED_WIRE_12;
-wire	SYNTHESIZED_WIRE_54;
-wire	SYNTHESIZED_WIRE_55;
-wire	SYNTHESIZED_WIRE_56;
 wire	SYNTHESIZED_WIRE_16;
 wire	SYNTHESIZED_WIRE_22;
 wire	SYNTHESIZED_WIRE_25;
@@ -60,38 +58,35 @@ wire	SYNTHESIZED_WIRE_36;
 wire	SYNTHESIZED_WIRE_37;
 wire	SYNTHESIZED_WIRE_38;
 wire	SYNTHESIZED_WIRE_39;
-wire	SYNTHESIZED_WIRE_40;
-wire	SYNTHESIZED_WIRE_41;
-wire	SYNTHESIZED_WIRE_42;
 
 
 
 
-assign	SYNTHESIZED_WIRE_37 = carry_in & SYNTHESIZED_WIRE_43 & SYNTHESIZED_WIRE_44 & SYNTHESIZED_WIRE_45 & SYNTHESIZED_WIRE_46 & SYNTHESIZED_WIRE_47 & SYNTHESIZED_WIRE_5 & SYNTHESIZED_WIRE_48;
+assign	SYNTHESIZED_WIRE_34 = carry_in & SYNTHESIZED_WIRE_40 & SYNTHESIZED_WIRE_41 & SYNTHESIZED_WIRE_42 & SYNTHESIZED_WIRE_43 & SYNTHESIZED_WIRE_44 & SYNTHESIZED_WIRE_5 & SYNTHESIZED_WIRE_45;
 
-assign	SYNTHESIZED_WIRE_54 = SYNTHESIZED_WIRE_49 & SYNTHESIZED_WIRE_50 & SYNTHESIZED_WIRE_51 & SYNTHESIZED_WIRE_52 & SYNTHESIZED_WIRE_53 & SYNTHESIZED_WIRE_12;
+assign	SYNTHESIZED_WIRE_51 = SYNTHESIZED_WIRE_46 & SYNTHESIZED_WIRE_47 & SYNTHESIZED_WIRE_48 & SYNTHESIZED_WIRE_49 & SYNTHESIZED_WIRE_50 & SYNTHESIZED_WIRE_12;
 
-assign	SYNTHESIZED_WIRE_41 = SYNTHESIZED_WIRE_54 & SYNTHESIZED_WIRE_55 & SYNTHESIZED_WIRE_56 & SYNTHESIZED_WIRE_16;
+assign	SYNTHESIZED_WIRE_38 = SYNTHESIZED_WIRE_51 & SYNTHESIZED_WIRE_52 & SYNTHESIZED_WIRE_53 & SYNTHESIZED_WIRE_16;
 
 
 inc_dec_2bit	b2v_dual_adder_0(
 	.carry_borrow_in(carry_in),
 	.d1_in(d[1]),
 	.d0_in(d[0]),
-	.dec1_in(SYNTHESIZED_WIRE_43),
-	.dec0_in(SYNTHESIZED_WIRE_44),
+	.dec1_in(SYNTHESIZED_WIRE_40),
+	.dec0_in(SYNTHESIZED_WIRE_41),
 	.carry_borrow_out(SYNTHESIZED_WIRE_22),
 	.d1_out(address_ALTERA_SYNTHESIZED[1]),
 	.d0_out(address_ALTERA_SYNTHESIZED[0]));
 
 
 inc_dec_2bit	b2v_dual_adder_10(
-	.carry_borrow_in(SYNTHESIZED_WIRE_54),
+	.carry_borrow_in(SYNTHESIZED_WIRE_51),
 	.d1_in(d[13]),
 	.d0_in(d[12]),
-	.dec1_in(SYNTHESIZED_WIRE_56),
-	.dec0_in(SYNTHESIZED_WIRE_55),
-	.carry_borrow_out(SYNTHESIZED_WIRE_40),
+	.dec1_in(SYNTHESIZED_WIRE_53),
+	.dec0_in(SYNTHESIZED_WIRE_52),
+	.carry_borrow_out(SYNTHESIZED_WIRE_37),
 	.d1_out(address_ALTERA_SYNTHESIZED[13]),
 	.d0_out(address_ALTERA_SYNTHESIZED[12]));
 
@@ -100,8 +95,8 @@ inc_dec_2bit	b2v_dual_adder_2(
 	.carry_borrow_in(SYNTHESIZED_WIRE_22),
 	.d1_in(d[3]),
 	.d0_in(d[2]),
-	.dec1_in(SYNTHESIZED_WIRE_48),
-	.dec0_in(SYNTHESIZED_WIRE_45),
+	.dec1_in(SYNTHESIZED_WIRE_45),
+	.dec0_in(SYNTHESIZED_WIRE_42),
 	.carry_borrow_out(SYNTHESIZED_WIRE_25),
 	.d1_out(address_ALTERA_SYNTHESIZED[3]),
 	.d0_out(address_ALTERA_SYNTHESIZED[2]));
@@ -111,19 +106,19 @@ inc_dec_2bit	b2v_dual_adder_4(
 	.carry_borrow_in(SYNTHESIZED_WIRE_25),
 	.d1_in(d[5]),
 	.d0_in(d[4]),
-	.dec1_in(SYNTHESIZED_WIRE_46),
-	.dec0_in(SYNTHESIZED_WIRE_47),
-	.carry_borrow_out(SYNTHESIZED_WIRE_42),
+	.dec1_in(SYNTHESIZED_WIRE_43),
+	.dec0_in(SYNTHESIZED_WIRE_44),
+	.carry_borrow_out(SYNTHESIZED_WIRE_39),
 	.d1_out(address_ALTERA_SYNTHESIZED[5]),
 	.d0_out(address_ALTERA_SYNTHESIZED[4]));
 
 
 inc_dec_2bit	b2v_dual_adder_7(
-	.carry_borrow_in(SYNTHESIZED_WIRE_50),
+	.carry_borrow_in(SYNTHESIZED_WIRE_47),
 	.d1_in(d[8]),
 	.d0_in(d[7]),
-	.dec1_in(SYNTHESIZED_WIRE_49),
-	.dec0_in(SYNTHESIZED_WIRE_51),
+	.dec1_in(SYNTHESIZED_WIRE_46),
+	.dec0_in(SYNTHESIZED_WIRE_48),
 	.carry_borrow_out(SYNTHESIZED_WIRE_31),
 	.d1_out(address_ALTERA_SYNTHESIZED[8]),
 	.d0_out(address_ALTERA_SYNTHESIZED[7]));
@@ -133,61 +128,53 @@ inc_dec_2bit	b2v_dual_adder_9(
 	.carry_borrow_in(SYNTHESIZED_WIRE_31),
 	.d1_in(d[10]),
 	.d0_in(d[9]),
-	.dec1_in(SYNTHESIZED_WIRE_53),
-	.dec0_in(SYNTHESIZED_WIRE_52),
-	.carry_borrow_out(SYNTHESIZED_WIRE_39),
+	.dec1_in(SYNTHESIZED_WIRE_50),
+	.dec0_in(SYNTHESIZED_WIRE_49),
+	.carry_borrow_out(SYNTHESIZED_WIRE_36),
 	.d1_out(address_ALTERA_SYNTHESIZED[10]),
 	.d0_out(address_ALTERA_SYNTHESIZED[9]));
 
-assign	SYNTHESIZED_WIRE_36 =  ~address_ALTERA_SYNTHESIZED[0];
+assign	SYNTHESIZED_WIRE_47 = SYNTHESIZED_WIRE_34 & SYNTHESIZED_WIRE_35;
 
-assign	address_is_1 = ~(SYNTHESIZED_WIRE_34 | SYNTHESIZED_WIRE_35);
+assign	SYNTHESIZED_WIRE_35 =  ~limit6;
 
-assign	SYNTHESIZED_WIRE_34 = address_ALTERA_SYNTHESIZED[7] | address_ALTERA_SYNTHESIZED[5] | address_ALTERA_SYNTHESIZED[6] | address_ALTERA_SYNTHESIZED[4] | address_ALTERA_SYNTHESIZED[2] | address_ALTERA_SYNTHESIZED[3] | address_ALTERA_SYNTHESIZED[1] | SYNTHESIZED_WIRE_36;
+assign	SYNTHESIZED_WIRE_41 = d[0] ^ decrement;
 
-assign	SYNTHESIZED_WIRE_35 = address_ALTERA_SYNTHESIZED[15] | address_ALTERA_SYNTHESIZED[13] | address_ALTERA_SYNTHESIZED[14] | address_ALTERA_SYNTHESIZED[12] | address_ALTERA_SYNTHESIZED[10] | address_ALTERA_SYNTHESIZED[11] | address_ALTERA_SYNTHESIZED[9] | address_ALTERA_SYNTHESIZED[8];
+assign	SYNTHESIZED_WIRE_40 = d[1] ^ decrement;
 
-assign	SYNTHESIZED_WIRE_50 = SYNTHESIZED_WIRE_37 & SYNTHESIZED_WIRE_38;
-
-assign	SYNTHESIZED_WIRE_38 =  ~limit6;
-
-assign	SYNTHESIZED_WIRE_44 = d[0] ^ decrement;
-
-assign	SYNTHESIZED_WIRE_43 = d[1] ^ decrement;
-
-assign	SYNTHESIZED_WIRE_53 = d[10] ^ decrement;
+assign	SYNTHESIZED_WIRE_50 = d[10] ^ decrement;
 
 assign	SYNTHESIZED_WIRE_12 = d[11] ^ decrement;
 
-assign	address_ALTERA_SYNTHESIZED[11] = SYNTHESIZED_WIRE_39 ^ d[11];
+assign	address_ALTERA_SYNTHESIZED[11] = SYNTHESIZED_WIRE_36 ^ d[11];
 
-assign	SYNTHESIZED_WIRE_55 = d[12] ^ decrement;
+assign	SYNTHESIZED_WIRE_52 = d[12] ^ decrement;
 
-assign	SYNTHESIZED_WIRE_56 = d[13] ^ decrement;
+assign	SYNTHESIZED_WIRE_53 = d[13] ^ decrement;
 
 assign	SYNTHESIZED_WIRE_16 = d[14] ^ decrement;
 
-assign	address_ALTERA_SYNTHESIZED[14] = SYNTHESIZED_WIRE_40 ^ d[14];
+assign	address_ALTERA_SYNTHESIZED[14] = SYNTHESIZED_WIRE_37 ^ d[14];
 
-assign	address_ALTERA_SYNTHESIZED[15] = SYNTHESIZED_WIRE_41 ^ d[15];
+assign	address_ALTERA_SYNTHESIZED[15] = SYNTHESIZED_WIRE_38 ^ d[15];
 
-assign	SYNTHESIZED_WIRE_45 = d[2] ^ decrement;
+assign	SYNTHESIZED_WIRE_42 = d[2] ^ decrement;
 
-assign	SYNTHESIZED_WIRE_48 = d[3] ^ decrement;
+assign	SYNTHESIZED_WIRE_45 = d[3] ^ decrement;
 
-assign	SYNTHESIZED_WIRE_47 = d[4] ^ decrement;
+assign	SYNTHESIZED_WIRE_44 = d[4] ^ decrement;
 
-assign	SYNTHESIZED_WIRE_46 = d[5] ^ decrement;
+assign	SYNTHESIZED_WIRE_43 = d[5] ^ decrement;
 
 assign	SYNTHESIZED_WIRE_5 = d[6] ^ decrement;
 
-assign	address_ALTERA_SYNTHESIZED[6] = SYNTHESIZED_WIRE_42 ^ d[6];
+assign	address_ALTERA_SYNTHESIZED[6] = SYNTHESIZED_WIRE_39 ^ d[6];
 
-assign	SYNTHESIZED_WIRE_51 = d[7] ^ decrement;
+assign	SYNTHESIZED_WIRE_48 = d[7] ^ decrement;
 
-assign	SYNTHESIZED_WIRE_49 = d[8] ^ decrement;
+assign	SYNTHESIZED_WIRE_46 = d[8] ^ decrement;
 
-assign	SYNTHESIZED_WIRE_52 = d[9] ^ decrement;
+assign	SYNTHESIZED_WIRE_49 = d[9] ^ decrement;
 
 assign	address = address_ALTERA_SYNTHESIZED;
 
