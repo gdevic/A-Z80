066c3cad0f130c8b0966abfc48d197a0fa1b908c
Goran Devic <gdevic@yahoo.com>
2014-09-04 09:12:47 -0500

    tools: Changed bindump to a nicer output

--- tools/zmac/bindump.py
+++ tools/zmac/bindump.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # This script reads a binary file and writes it out in the ASCII format
-# that lists each 8-bit word, in a hex format without 0x, on a separate line.
+# that lists each 8-bit word in a hex format without 0x.
 #
 # This format can be read by SystemVerilog function $readmemh()
 #
@@ -11,11 +11,16 @@ import sys
 
 filename = sys.argv[1]
 outfile = "out.hex"
+col = 0;
 if len(sys.argv) > 2:
     outfile  = sys.argv[2]
 with open(filename, "rb") as f, open(outfile, "w") as o:
     block = f.read(65536)
     for ch in block:
         #print '{0:02X}'.format(ord(ch))
-        o.write('{0:02X}'.format(ord(ch))+"\n")
+        o.write('{0:02X} '.format(ord(ch)))
+        col = col + 1
+        if col==16:
+            o.write("\n")
+            col = 0;
 print "Created " + outfile
