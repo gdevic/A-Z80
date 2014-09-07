4d75ed0a43b73673173d2b73866bd1bb0f31badd
Goran Devic <gdevic@yahoo.com>
2014-09-07 12:36:04 -0500

    z80: Fixing the tb_io - no need to count even chars
    - Guess the glitch removal fixed this

--- cpu/toplevel/tb_io.sv
+++ cpu/toplevel/tb_io.sv
@@ -16,10 +16,6 @@ reg [7:0] IO [0:1<<16];
 // Return data at the specified IO address; return 0xFF for non-initialized IO
 assign Data = (!CS && !OE) ? (IO[Address]===8'hxx) ? 8'hFF : IO[Address] : {8{1'bz}};
 
-// When printing a text through this IO service, work around the
-// fact that IO lasts 2T so we would be printing on each clock
-// and thus see duplicate characters
-int even = 0;
 int fd;
 
 // Read the initial content of the IO map from file
@@ -42,17 +38,13 @@ always @(CS or WE)
         if (debug)
             $strobe("[IO] OUT A=%H, D=%H", Address, Data);
         if (Address==2222) begin
-            // Print only every 2T
-            if (even) begin
-                $write("%c", Data);
-                // If logging to a file was enabled, append a character
-                if (iolog) begin
-                    fd = $fopen("iolog.txt", "ab");
-                    $fwrite(fd, "%c", Data);
-                    $fclose(fd);
-                end
+            $write("%c", Data);
+            // If logging to a file was enabled, append a character
+            if (iolog) begin
+                fd = $fopen("iolog.txt", "ab");
+                $fwrite(fd, "%c", Data);
+                $fclose(fd);
             end
-            even = even ^ 1;
         end
         IO[Address] = Data;
     end
