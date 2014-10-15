a8b4836fad31070861d574dff34f6be8f7d3ee27
Goran Devic <gdevic@yahoo.com>
2014-10-15 09:19:38 -0500

    ula: Use flash instead of internal memory
    - Also adding the clocks block

---
+++ host/ula/clocks.sv
@@ -0,0 +1,30 @@
+//============================================================================
+// Implementation of the Sinclair ZX Spectrum ULA
+//
+// This module contains the clocks section.
+//
+// TODO: Video RAM contention would cause a clock gating implemented in this
+//       module.
+//============================================================================
+module clocks
+(
+    input wire clk_ula,         // Input ULA clock of 14 MHz
+    output reg clk_cpu          // Output 3.5 MHz CPU clock
+);
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Generate 3.5 MHz Z80 CPU clock by dividing input clock of 14 MHz by 4
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+reg div2;                       // Divide input clock by 2
+
+always @(posedge clk_ula)
+begin
+    div2 <= !div2;
+end
+
+always @(posedge div2)
+begin
+    clk_cpu = !clk_cpu;         // Divide /2 clock by 2
+end
+
+endmodule
--- host/ula/test_ula.sv
+++ host/ula/test_ula.sv
@@ -3,7 +3,8 @@
 //============================================================================
 module test_ula
 (
-    input wire CLOCK_27,            // Input clock 27 MHz
+    input wire CLOCK_27,        // Input clock 27 MHz
+    input wire KEY0,            // Button 0 is reset
 
     output wire [3:0] VGA_R,
     output wire [3:0] VGA_G,
@@ -11,26 +12,37 @@ module test_ula
     output reg VGA_HS,
     output reg VGA_VS,
 
-    output wire [5:0] GPIO_0,       // Scope test points
+    output wire [21:0] FL_ADDR,
+    input wire [7:0] FL_DQ,
+    output wire FL_CE_N,
+    output wire FL_OE_N,
+    output wire FL_RST_N,
+    output wire FL_WE_N,
+    
+    output wire [6:0] GPIO_0,   // Scope test points
     input wire SW0,
     input wire SW1,
     input wire SW2
 );
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Instantiate PLL
+// Instantiate PLL and clocks block
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 wire clk_pix;                   // VGA pixel clock (25.175 MHz)
 wire clk_ula;                   // ULA master clock (14 MHz)
 pll pll_( .inclk0(CLOCK_27), .c0(clk_pix), .c1(clk_ula) );
 
+wire clk_cpu;                   // Clocks generates CPU clocks of 3.5 MHz
+clocks clocks_( .* );
+
 // Various scope test points
 assign GPIO_0[0] = CLOCK_27;
 assign GPIO_0[1] = clk_pix;
 assign GPIO_0[2] = clk_ula;
-assign GPIO_0[3] = VGA_VS;
-assign GPIO_0[4] = VGA_HS;
-assign GPIO_0[5] = VGA_B[0];
+assign GPIO_0[3] = clk_cpu;
+assign GPIO_0[4] = VGA_VS;
+assign GPIO_0[5] = VGA_HS;
+assign GPIO_0[6] = VGA_B[0];
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Instantiate RAM that contains a sample screen image
@@ -51,7 +63,14 @@ assign border = { SW2, SW1, SW0 };
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Instantiate ULA's video subsystem
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-video video_( .* );
+//video video_( .*, .vram_address(vram_address), .vram_data(vram_data) );
 
+// Use flash interface instead of the internal RAM
+assign FL_CE_N = 0;
+assign FL_OE_N = 0;
+assign FL_RST_N = KEY0;
+assign FL_WE_N = 1;
+assign FL_ADDR[21:13] = 'b10;
+video video_( .*, .vram_address(FL_ADDR[12:0]), .vram_data(FL_DQ) );
 
 endmodule
