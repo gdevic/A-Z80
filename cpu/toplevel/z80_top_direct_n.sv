f2e4a2a261765dc9c789b7b5812dd1f4004f2659
Goran Devic <gdevic@yahoo.com>
2016-02-08 20:38:06 -0600

    z80_top_direct.sv is now Verilog.

--- cpu/toplevel/z80_top_direct_n.sv
+++ cpu/toplevel/z80_top_direct_n.sv
@@ -32,23 +32,54 @@ module z80_top_direct_n(
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Address, Data and Control bus drivers connecting to external pins
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-address_pins   address_pins_( .*, .abus(A[15:0]) );
-data_pins      data_pins_   ( .*, .db(db0[7:0]), .D(D[7:0]) );
-control_pins_n control_pins_( .*,
-    .pin_nM1      (nM1),
-    .pin_nMREQ    (nMREQ),
-    .pin_nIORQ    (nIORQ),
-    .pin_nRD      (nRD),
-    .pin_nWR      (nWR),
-    .pin_nRFSH    (nRFSH),
-    .pin_nHALT    (nHALT),
-    .pin_nWAIT    (nWAIT),
-    .pin_nBUSACK  (nBUSACK),
-    .pin_nINT     (nINT),
-    .pin_nNMI     (nNMI),
-    .pin_nRESET   (nRESET),
-    .pin_nBUSRQ   (nBUSRQ),
-    .CPUCLK       (CLK)
+address_pins address_pins_(
+    .clk            (clk),
+    .bus_ab_pin_we  (bus_ab_pin_we),
+    .pin_control_oe (pin_control_oe),
+    .address        (address),
+    .abus           (A)
+);
+
+data_pins data_pins_(
+    .bus_db_pin_oe  (bus_db_pin_oe),
+    .bus_db_pin_re  (bus_db_pin_re),
+    .ctl_bus_db_we  (ctl_bus_db_we),
+    .bus_db_oe      (bus_db_oe),
+    .clk            (clk),
+    .db             (db0),
+    .D              (D)
+);
+
+control_pins_n control_pins_(
+    .busack        (busack),
+    .CPUCLK        (CLK),
+    .pin_control_oe(pin_control_oe),
+    .in_halt       (in_halt),
+    .pin_nWAIT     (nWAIT),
+    .pin_nBUSRQ    (nBUSRQ),
+    .pin_nINT      (nINT),
+    .pin_nNMI      (nNMI),
+    .pin_nRESET    (nRESET),
+    .nM1_out       (nM1_out),
+    .nRFSH_out     (nRFSH_out),
+    .nRD_out       (nRD_out),
+    .nWR_out       (nWR_out),
+    .nIORQ_out     (nIORQ_out),
+    .nMREQ_out     (nMREQ_out),
+    .nmi           (nmi),
+    .busrq         (busrq),
+    .clk           (clk),
+    .intr          (intr),
+    .mwait         (mwait),
+    .reset_in      (reset_in),
+    .pin_nM1       (nM1),
+    .pin_nMREQ     (nMREQ),
+    .pin_nIORQ     (nIORQ),
+    .pin_nRD       (nRD),
+    .pin_nWR       (nWR),
+    .pin_nRFSH     (nRFSH),
+    .pin_nHALT     (nHALT),
+    .pin_nBUSACK   (nBUSACK)
  );
 
 endmodule
