1e3059008cf692223059dd0c581918e6d23bdbbe
Goran Devic <gdevic@yahoo.com>
2016-02-14 21:17:04 -0600

    Adding scripts to convert, generate Xilinx coe and mif files Plugged them into the workflow to support basic nexys3 and ISim

--- tools/zmac/make_fpga.bat
+++ tools/zmac/make_fpga.bat
@@ -1,8 +1,10 @@
 @echo off
 Rem
-Rem     Creates an Intel HEX file format from a Z80 source file.
-Rem     This hex file is loaded into the ROM module for both the ModelSim
-Rem     and to be included into the target FPGA data file at the "host" level.
+Rem     Assembles Z80 source file into object code and generates various
+Rem     formats of that code to be used by FPGA test projects.
+Rem
+Rem     Intel HEX files are used by ModelSim and Altera synthesis,
+Rem     COE and MIF files are used by Xilinx and its ISim simulation.
 Rem
 Rem     Give it an argument of the ASM file you want to use, or you can simply drag
 Rem     and drop an asm file into it. If you drop an ASM file and there were errors,
@@ -12,11 +14,18 @@ zmac.exe --zmac %1
 if errorlevel 1 goto error
 bin2hex.exe zout\%~n1.cim fpga.hex
 if errorlevel 1 goto error
+python bin2coe.py zout\%~n1.cim ram.coe
+if errorlevel 1 goto error
+python bin2mif.py zout\%~n1.cim ram.mif
+if errorlevel 1 goto error
 
 Rem     Copy hex files to their target Quartus/ModelSim host directories
 copy /Y fpga.hex ..\..\host\basic_de1
 copy /Y fpga.hex ..\..\host\basic_de1\simulation\modelsim
 
+Rem     Copy .mif and .coe files to their target Xilinx host directory
+copy /Y ram.coe ..\..\host\basic_nexys3
+copy /Y ram.mif ..\..\host\basic_nexys3
 goto end
 
 :error
