36fe25310b09bd4c3eed0afc59fdbe3676bf5bca
Goran Devic <gdevic@yahoo.com>
2016-03-07 19:25:47 -0600

    z80: Adding Python script to copy project files to user folder
    - Updated list of files to include all final Verilog files

---
+++ cpu/export.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+#
+# This script exports all core A-Z80 Verilog files to a destination of your choice.
+# The files copied are necessary and sufficient to include with your custom project.
+#
+#-------------------------------------------------------------------------------
+#  Copyright (C) 2016  Goran Devic
+#
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the Free
+#  Software Foundation; either version 2 of the License, or (at your option)
+#  any later version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#-------------------------------------------------------------------------------
+import sys
+import os
+from shutil import copyfile
+
+if len(sys.argv) != 2:
+    print ("\nUsage: export.py <destination-folder>\n")
+    print ("Copies all core A-Z80 Verilog files to a destination of your choice.")
+    print ("The files copied are necessary and sufficient to include with your project.")
+    exit(-1)
+
+dest = sys.argv[1]
+total = 0
+
+if not os.path.exists(dest):
+    print ("ERROR: Destination folder does not exist!")
+    exit(-1)
+
+if not os.path.isdir(dest):
+    print ("ERROR: Destination is not a directory!")
+    exit(-1)
+
+with open('top-level-files.txt') as f:
+    files = f.read().splitlines()
+
+# Read and copy each file from the list of input files
+for infile in files:
+    if infile.startswith('+'):
+        infile = infile[2:]
+    if infile.startswith('Files='):
+        files = int(infile[6:])
+        if total != files:
+            print ("ERROR: Incorrect number of files copied!")
+            exit(-1)
+        else:
+            print ("\nDone copying {0} files.".format(files))
+    if not os.path.isfile(infile):
+        continue
+    name = os.path.basename(infile)
+    print ('Copying', infile)
+    copyfile(infile, dest + '/' + name)
+    total += 1
